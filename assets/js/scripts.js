09d40b16ebb46a36a469efca7e945de939e0c40f
Tobias Hartmann <login@hartmanntobias.de>
2023-03-02 16:25:18 +0100

    hovering menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,6 +20,28 @@ document.getElementById("toc-placeholder").appendChild(ul);
 
 
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
 // BACK TO TOP HOVER BUTTON
 
 window.onscroll = function() {scrollFunction()};
@@ -38,3 +60,5 @@ document.querySelector('.back-to-top').addEventListener('click', function(e) {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 	});
+
+
